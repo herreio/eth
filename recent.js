971625eceef0aeef9f8d264716cf1113bb564ed5
herreio <mail@herre.io>
2019-04-23 11:54:06 +0200

    improve feed presentation

--- recent.js
+++ recent.js
@@ -64,6 +64,7 @@ function createHome(){
 
 function initContent(xmlFeed){
     var i = 0;
+    var empty = '\n';
     var content = "";
     var feed = xmlFeed.getElementsByTagName("feed").item(0);
     if (feed == null) {
@@ -73,16 +74,22 @@ function initContent(xmlFeed){
     var entries = feed.getElementsByTagName("entry").length;
     for (i = 0; i < entries; ++i) {
         var entry = feed.getElementsByTagName('entry').item(i);
-        var title = entry.getElementsByTagName('title').item(0).firstChild.data;
-        /*  if (title.length > 79) title = title.substr(0,77) + "..."; */
-        var creator = entry.getElementsByTagName('dc:creator').item(0).innerHTML;
         var published = entry.getElementsByTagName('published').item(0).innerHTML;
         var date = new Date(published);
         var dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
-        published = date.toLocaleDateString(language,dateOptions) + '   ';
+        published = date.toLocaleDateString(language,dateOptions) + ' (';
         published += date.toLocaleTimeString(language);
+        content += published + ')' + empty;
+        /* var idx = entry.getElementsByTagName('id').item(0).firstChild.data.split("/");
+        idx = idx[idx.length-1];
+        content += idx + empty; */
         var link = entry.getElementsByTagName('link').item(0).getAttribute("href");
-        content += creator + '\n' + '<a target="_blank" href="' + link +'">' + title + '</a>\n' + published + '\n\n';
+        var title = entry.getElementsByTagName('title').item(0).firstChild.data;
+        /*  if (title.length > 79) title = title.substr(0,77) + "..."; */
+        content += '<a target="_blank" href="' + link +'"><i>' + title + '</i></a>' + empty;
+        var creator = entry.getElementsByTagName('dc:creator').item(0).innerHTML;
+        content += creator + empty + empty;
+
     }
     document.getElementById('content').innerHTML += content;
 }
